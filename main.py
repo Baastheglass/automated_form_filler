0cec4c7f481d3df6f109fa51ff2d778259db55f4
Baastheglass <lightningblazer123@gmail.com>
2025-07-15 01:05:50 +0500

    page one of the form surpassed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
+import time
+import pyautogui
 
 if __name__ == "__main__":
     load_dotenv()
@@ -9,3 +14,10 @@ if __name__ == "__main__":
     driver = uc.Chrome()
 
     driver.get(form_link)
+    driver.maximize_window()
+    time.sleep(5)
+    pyautogui.click(673, 525)
+    print("Clicked!")
+    driver.find_element(By.CSS_SELECTOR, "#mG61Hd > div.RH5hzf.RLS9Fe > div > div.ThHDze > div.DE3NNc.CekdCb > div.lRwqcd > div").click()
+    print("Clicked next!")
+    time.sleep(5)
